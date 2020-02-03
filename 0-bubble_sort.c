adeccb229bc2222415de62b5ba9e8835c1406467
Camilo Morales <camiloandres.1509@gmail.com>
2020-02-02 21:01:23 -0500

    Adding size condition

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,21 +9,23 @@ void bubble_sort(int *array, size_t size)
 {
 	size_t i, a = size, n = size, tem = 0;
 
-	size--;
-	while (a != 0)
+	if (size >= 2)
 	{
-		for (i = 0; i + 1 <= size; i++)
+		size--;
+		while (a != 0)
 		{
-			if (array[i] > array[i + 1])
+			for (i = 0; i + 1 <= size; i++)
 			{
-				tem = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = tem;
-				print_array(array, n);
+				if (array[i] > array[i + 1])
+				{
+					tem = array[i];
+					array[i] = array[i + 1];
+					array[i + 1] = tem;
+					print_array(array, n);
+				}
 			}
+			a = size--;
+			i = 0;
 		}
-		a = size--;
-		i = 0;
 	}
 }
-
